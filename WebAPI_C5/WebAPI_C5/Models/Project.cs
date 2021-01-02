2324ee825be880aa08a8e6eb79f3a6ab23eff276
Souhail <souhaildq@gmail.com>
2021-01-02 17:56:00 +0100

    back end changes

--- WebAPI_C5/WebAPI_C5/Models/Project.cs
+++ WebAPI_C5/WebAPI_C5/Models/Project.cs
@@ -9,8 +9,7 @@ namespace WebAPI_C5.Models
     {
         public int ProjectId { get; set; }
         public string ProjectName { get; set; }
-
-        public ICollection<Consultant> Consultants { get; set; }
+        public List<Affectation> Affectations { get; set; }
     }
 }
 
